f8623a608694971e11708b93ef7a101679f62d9e
mattiefm <mmfuller22@gmail.com>
2024-04-01 10:55:40 -0600

    testing higlass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 
+
 import './css/master_style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/App.css';
 import { WebRoutes } from './Route.js';
 import { AppNavbar } from "./components/navbar/Navbar";
 import { Footer } from "./components/Footer/footer";
-
 
 function App() {
   return (
